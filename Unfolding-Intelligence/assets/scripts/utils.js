1585d0057efc86a5878a7e0c59070e73a0a9f229
stc <stc@binaura.net>
2020-10-27 19:06:38 +0100

    sound components moved to utils

--- Unfolding-Intelligence/assets/scripts/utils.js
+++ Unfolding-Intelligence/assets/scripts/utils.js
@@ -49,3 +49,35 @@ const colors = {
   negative_food: [46,42,21],
   type: [64,80,85,220]
 }
+
+// tonejs components
+
+let panner = new Tone.Panner(-1).toDestination();
+console.log(Tone.FMSynth);
+let fm = new Tone.FMSynth({
+    "harmonicity": 18,
+    "modulationIndex": 8,
+    "detune": 0,
+    "oscillator": {
+        "type": "square"
+    },
+    "envelope": {
+        "attack": 0.01,
+        "decay": 0.02,
+        "sustain": 0.15,
+        "release": 2
+    },
+    "modulation": {
+        "type": "square"
+    },
+    "modulationEnvelope": {
+        "attack": 0.01,
+        "decay": 0.02,
+        "sustain": 0.03,
+        "release": 0.3
+    },
+    
+}).toDestination();
+fm.connect(panner);
+fm.volume.value = -14;
+panner.pan.value = 0.5;
