bfcb9f3e0efcfadd62c15f8175b1e16472a01aab
Agoston Nagy <stc@binaura.net>
2024-02-26 13:04:58 +0100

    keys "if" >> "switch"

--- RoArm-M2/node/main.js
+++ RoArm-M2/node/main.js
@@ -6,9 +6,6 @@ import { armDownSequence } from './Utils.js';
 const httpBridge = new HttpBridge();
 httpBridge.wristMode();
 
-
-
-
 // interact
 readline.emitKeypressEvents(process.stdin)
 if (process.stdin.isTTY) {
@@ -16,69 +13,58 @@ if (process.stdin.isTTY) {
 }
 process.stdin.on('keypress', (chunk, key) => {
     console.log(key.name);
-    if(key && key.name == 'space') {
-        httpBridge.moveInit();
-    }
-    if(key && key.name == 'q') {
-        httpBridge.wristMode();
-    }
-    if(key && key.name == 'g') {
-        httpBridge.getData();
-    }
-
-    if(key && key.name == '0') {
-        httpBridge.moveWristUp();
-    }
-
-    if(key && key.name == '1') {
-        httpBridge.moveRect(0);
-    }
-
-    if(key && key.name == '2') {
-        httpBridge.moveRect(1);
-    }
-
-    if(key && key.name == '3') {
-        httpBridge.moveRect(2);
-    }
-
-    if(key && key.name == '4') {
-        httpBridge.moveRect(3);
-    }
-
-    if(key && key.name == 'a') {
-        httpBridge.moveRectDown(0);
-    }
-
-    if(key && key.name == 's') {
-        httpBridge.moveRectDown(1);
-    }
-
-    if(key && key.name == 'd') {
-        httpBridge.moveRectDown(2);
-    }
-
-    if(key && key.name == 'f') {
-        httpBridge.moveRectDown(3);
-    }
-
-    if(key && key.name == 'k') {
-        httpBridge.moveSequence( armBaseSequence );
-    }
-
-    if(key && key.name == 't') {
-        httpBridge.takePaint();
-    }
-
-    if(key && key.name == 'y') {
-        httpBridge.centerBrush();
-    }
-
-    if(key && key.name == 'u') {
-        httpBridge.moveSequence( armDownSequence );
-    }
-    
-    if(key && key.name == 'escape') {
-        process.exit();
+    switch (key.name) {
+        case 'space':
+            httpBridge.moveInit();
+            break;
+        case 'q':
+            httpBridge.wristMode();
+            break;
+        case 'g':
+            httpBridge.getData();
+            break;
+        case '0':
+            httpBridge.moveWristUp();
+            break;
+        case '1':
+            httpBridge.moveRect(0);
+            break;
+        case '2':
+            httpBridge.moveRect(1);
+            break;
+        case '3':
+            httpBridge.moveRect(2);
+            break;  
+        case '4':
+            httpBridge.moveRect(3);
+            break;
+        case 'a':
+            httpBridge.moveRectDown(0);
+            break;
+        case 's':
+            httpBridge.moveRectDown(1);
+            break;
+        case 'd':
+            httpBridge.moveRectDown(2);
+            break;
+        case 'f':
+            httpBridge.moveRectDown(3);
+            break;
+        case 'k':
+            httpBridge.moveSequence( armBaseSequence );
+            break;
+        case 't':
+            httpBridge.takePaint();
+            break;
+        case 'y':
+            httpBridge.centerBrush();
+            break;
+        case 'u':
+            httpBridge.moveSequence( armDownSequence );
+            break;
+        case 'escape':
+            process.exit();    
+        default:
+            break;
     }
 });
