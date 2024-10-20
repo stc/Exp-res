551ff1683a53934ff5dfefc5195f9a7b7a14cf3d
Agoston Nagy <stc@binaura.net>
2024-10-20 10:45:18 +0200

    cleaning up

--- Quantum-Circuits/BinaryRandom.py
+++ Quantum-Circuits/BinaryRandom.py
@@ -12,8 +12,13 @@ print(circuit)
 service = QiskitRuntimeService(channel="ibm_quantum", token="e97cb70abdf2b346d87c00cbdf142528e7fcc8f07eb6dadc49e3318e8c5346b95494e2612ecb020af0c76dc410804251ba12eb1bad49e2110e5c86cb7b28bff4")
 backend = service.least_busy(operational=True, simulator=False, memory=True)
 transpiled_qc = transpile(circuit, backend)
-execute = backend.run([transpiled_qc], shots=1000, memory=True)
 
+# up to date query
 # job = Sampler(backend).run([transpiled_qc], shots=1000)
 # result = job.result()
-# memory = result.get_memory()
+
+# deprecated query
+job = backend.run([transpiled_qc], shots=1000, memory=True)
+result = job.result()
+
+# analyze the results properly using the getResult script
--- Quantum-Circuits/getResult.py
+++ Quantum-Circuits/getResult.py
@@ -5,33 +5,18 @@ service = QiskitRuntimeService(
     instance='ibm-q/open/main',
     token='e97cb70abdf2b346d87c00cbdf142528e7fcc8f07eb6dadc49e3318e8c5346b95494e2612ecb020af0c76dc410804251ba12eb1bad49e2110e5c86cb7b28bff4'
 )
+
 # bell-state results
 #job = service.job('cw9t34c2802g0081nvq0')
 
 # binary-random results
 job = service.job('cwabp899ezk00085nz50')
+
 job_result = job.result()
 memory = job_result.get_memory()
-
 shots_array = []
-# Loop through each shot in the BitArray and print the results
+
 for i, shot in enumerate(memory):
-    # Convert the bitstring to a list of integers if needed
-    # bit_array = [int(bit) for bit in shot]
     shots_array.append(shot)
 
 print(shots_array)
-
-
-"""
-all_shots = []
-for bitstring, count in counts.items():
-    # Convert the bitstring to a list of integers
-    bit_array = [int(bit) for bit in bitstring]
-    # Repeat each bitstring for the number of times it occurred
-    all_shots.extend([bit_array] * count)
-
-# Loop through each shot
-for i, shot in enumerate(all_shots):
-    print(f"Shot {i+1}: {shot}")
-"""
