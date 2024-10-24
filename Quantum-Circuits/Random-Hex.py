f6b8437817916240bea7e21abdd9b3de4bc0d641
Agoston Nagy <stc@binaura.net>
2024-10-24 13:58:35 +0200

    random-hex circuit

---
+++ Quantum-Circuits/Random-Hex.py
@@ -0,0 +1,27 @@
+import qiskit
+from qiskit import QuantumRegister, QuantumCircuit, transpile
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
+
+qr = QuantumRegister(6) 
+cr = qiskit.ClassicalRegister(6)
+circuit = QuantumCircuit(qr, cr)
+circuit.h([0])
+circuit.h([1])
+circuit.h([2])
+circuit.h([3])
+circuit.h([4])
+circuit.h([5])
+
+circuit.measure_all()
+print(circuit)
+
+service = QiskitRuntimeService(channel="ibm_quantum", token="e97cb70abdf2b346d87c00cbdf142528e7fcc8f07eb6dadc49e3318e8c5346b95494e2612ecb020af0c76dc410804251ba12eb1bad49e2110e5c86cb7b28bff4")
+backend = service.least_busy(operational=True, simulator=False, memory=True)
+print(service.backends())
+
+transpiled_qc = transpile(circuit, backend)
+job = backend.run(transpiled_qc, shots=1000, memory=True)
+result = job.result()
+
+# analyze the results properly using the getResult script
+
