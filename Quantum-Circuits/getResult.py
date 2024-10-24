f306d559631a9913d3f10778646895f7216e67d7
Agoston Nagy <stc@binaura.net>
2024-10-24 21:51:26 +0200

    random-hex results

--- Quantum-Circuits/getResult.py
+++ Quantum-Circuits/getResult.py
@@ -6,9 +6,12 @@ service = QiskitRuntimeService(
     token='e97cb70abdf2b346d87c00cbdf142528e7fcc8f07eb6dadc49e3318e8c5346b95494e2612ecb020af0c76dc410804251ba12eb1bad49e2110e5c86cb7b28bff4'
 )
 
+#random-hex results
+job = service.job('cwd30n22802g008m6nfg')
+
 # bell-state results
-#job = service.job('cw9t34c2802g0081nvq0')
-job = service.job('cwabsrqjyrs0008h1b4g')
+# job = service.job('cw9t34c2802g0081nvq0')
+# job = service.job('cwabsrqjyrs0008h1b4g')
 
 # binary-random results
 # job = service.job('cwabp899ezk00085nz50')
