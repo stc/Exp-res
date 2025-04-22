3dc529c1b2f9f05138cfc5327ffcc1d7bd554e99
Agoston Nagy <stc@binaura.net>
2025-04-22 12:50:03 +0200

    exporting, recording

--- Quantum-Computing/QCA-Visualization/sketch.js
+++ Quantum-Computing/QCA-Visualization/sketch.js
@@ -43,7 +43,8 @@ void main() {
 		threshold += pow(4.0, float(N - n)) * (mod(p.y, 2.0) + 2.0 * mod(p.x + p.y, 2.0));
 	}
 	threshold = (threshold + 1.0) / pow(4.0, float(N)) - 1e-4;
-	vec3 color = brightness > threshold ? uColor : vec3(0.0,0.0,0.9);
+  //0.0,0.0,0.8
+	vec3 color = brightness > threshold ? uColor : vec3(1.0,1.0,1.0);
 	gl_FragColor = vec4(color, 1.0);
 }
 `;
@@ -51,6 +52,8 @@ void main() {
 let index = 0;
 let fadeColor = 0;
 let canFade = false;
+
+let unit = 10;
 
 function preload() {
   font = loadFont("IBMPlexSans-Medium.ttf");
@@ -61,15 +64,16 @@ function setup() {
   gfx = createGraphics(width, height);
   mShader = createShader(vert, frag);
   shader(mShader);
-  mShader.setUniform("uColor", [0.8,0.8,0.8]);
-
+  
+  mShader.setUniform("uColor", [0.0,0.0,0.0]);
+  let margin = 80;
   caPairs = [
-    new CAPair("RULE 30", "Q-GATE", r_30_binary, r_30_simulator, 50, 50, 10),
-    new CAPair("RULE 90", "Q-GATE", r_90_binary, r_90_simulator, 400, 50, 10),
-    new CAPair("RULE 101", "Q-GATE", r_101_binary, r_101_simulator, 50, 300, 10),
-    new CAPair("RULE 105", "Q-GATE", r_105_binary, r_105_simulator, 400, 300, 10),
-    new CAPair("RULE 150", "Q-GATE", r_150_binary, r_150_simulator, 50, 550, 10),
-    new CAPair("RULE 181", "Q-GATE", r_181_binary, r_181_simulator, 400, 550, 10),
+    new CAPair("RULE 30", "Q-GATE", r_30_binary, r_30_simulator, 50 + margin, 50 + margin, unit),
+    new CAPair("RULE 90", "Q-GATE", r_90_binary, r_90_simulator, 400 + margin, 50+ margin, unit),
+    new CAPair("RULE 101", "Q-GATE", r_101_binary, r_101_simulator, 50+ margin, 300+ margin, unit),
+    new CAPair("RULE 105", "Q-GATE", r_105_binary, r_105_simulator, 400+ margin, 300+ margin, unit),
+    new CAPair("RULE 150", "Q-GATE", r_150_binary, r_150_simulator, 50+ margin, 550+ margin, unit),
+    new CAPair("RULE 181", "Q-GATE", r_181_binary, r_181_simulator, 400+ margin, 550+ margin, unit),
   ];
 }
 
@@ -224,9 +228,9 @@ class CAPair {
   displayLabel(){
     const maxCol = this.binary[0].length;
     noStroke();
-    fill(0,0,200);
+    fill(255);
     textFont(font);
-    textSize(10);
+    textSize(unit);
     text(this.rule, this.x - this.s, this.y - this.s * 2);
     text(this.qRule, this.x - this.s + maxCol * this.s * 3, this.y - this.s * 2);
   }
@@ -237,13 +241,13 @@ class CAPair {
     noStroke();
     fill(255)
     rectMode(CENTER);
-    rect(maxCol * this.s * 2 + this.x, this.y - this.s, 10, 1);
-    rect(maxCol * this.s * 2 + this.x, this.y - this.s, 1, 10);
-    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, this.y - this.s, 10, 1);
-    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, this.y - this.s, 1, 10);
-    rect(maxCol * this.s * 2 + this.x, maxRow * this.s * 2 + this.y, 10, 1);
-    rect(maxCol * this.s * 2 + this.x, maxRow * this.s * 2 + this.y, 1, 10);
-    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, maxRow * this.s * 2 + this.y, 10, 1);
-    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, maxRow * this.s * 2 + this.y, 1, 10);
+    rect(maxCol * this.s * 2 + this.x, this.y - this.s, unit, 1);
+    rect(maxCol * this.s * 2 + this.x, this.y - this.s, 1, unit);
+    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, this.y - this.s, unit, 1);
+    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, this.y - this.s, 1, unit);
+    rect(maxCol * this.s * 2 + this.x, maxRow * this.s * 2 + this.y, unit, 1);
+    rect(maxCol * this.s * 2 + this.x, maxRow * this.s * 2 + this.y, 1, unit);
+    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, maxRow * this.s * 2 + this.y, unit, 1);
+    rect(maxCol * this.s * 2 + this.x + maxCol * this.s * 3, maxRow * this.s * 2 + this.y, 1, unit);
   }
 }
